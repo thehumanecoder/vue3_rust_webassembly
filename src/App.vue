e7a1796b1c778901840907620b215ffa76752083
Biswas Sampad <biswassampads@gmail.com>
2022-03-10 18:45:20 +0530

    done .

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <button type="button" @click="showAlert">Click Me</button>
+  </div>
+</template>
+
+<script>
+import {greet} from 'rustfiles'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  methods:{
+    showAlert(){
+      greet("Hello world from vue via rust webassembly")
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
